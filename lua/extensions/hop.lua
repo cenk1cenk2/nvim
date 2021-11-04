016e486315a7bec1b05e7e363e70baadebef5c74
Cenk <cenk@kilic.dev>
2021-11-04 13:15:00 +0100

    fix: add hop setup

--- lua/extensions/hop.lua
+++ lua/extensions/hop.lua
@@ -7,10 +7,15 @@ function M.config()
     active = true,
     on_config_done = nil,
     keymaps = { normal_mode = { ["s"] = ":HopChar2<cr>", ["S"] = ":HopWord<cr>" } },
+    setup = {},
   }
 end
 
 function M.setup()
+  local extension = require "hop"
+
+  extension.setup(lvim.extensions[extension_name].setup)
+
   require("lvim.keymappings").load(lvim.extensions[extension_name].keymaps)
 
   if lvim.extensions[extension_name].on_config_done then
