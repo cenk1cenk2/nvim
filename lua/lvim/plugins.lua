7f382d8e44f859f35f86a908de177f57e3f1378b
Cenk Kılıç <cenk@kilic.dev>
2021-11-16 21:22:38 +0100

    fix: plugin duplication

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -40,8 +40,6 @@ return {
   { "wbthomason/packer.nvim", commit = commit.packer },
   { "neovim/nvim-lspconfig", commit = commit.lsp_config },
   { "tamago324/nlsp-settings.nvim", commit = commit.nlsp_settings },
-  { "jose-elias-alvarez/null-ls.nvim", commit = commit.null_ls },
-  -- { "antoinemadec/FixCursorHold.nvim", commit = commit.fix_cursor_hold }, -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
   {
     "jose-elias-alvarez/null-ls.nvim",
     commit = commit.null_ls,
