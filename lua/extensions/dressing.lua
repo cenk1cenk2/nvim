741786ef74e0a836bd50770da2928a038babdd50
Cenk Kılıç <cenk@kilic.dev>
2021-12-11 19:40:50 +0100

    fix: update plugin config

--- lua/extensions/dressing.lua
+++ lua/extensions/dressing.lua
@@ -45,47 +45,6 @@ function M.config()
           theme = "dropdown",
         },
 
-        -- Options for fzf selector
-        fzf = {
-          window = {
-            width = 0.5,
-            height = 0.4,
-          },
-        },
-
-        -- Options for nui Menu
-        nui = {
-          position = "50%",
-          size = nil,
-          relative = "editor",
-          border = {
-            style = "rounded",
-          },
-          max_width = 80,
-          max_height = 40,
-        },
-
-        -- Options for built-in selector
-        builtin = {
-          -- These are passed to nvim_open_win
-          anchor = "NW",
-          relative = "cursor",
-          row = 0,
-          col = 0,
-          border = "rounded",
-
-          -- Window transparency (0-100)
-          winblend = 10,
-
-          -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-          width = nil,
-          max_width = 0.8,
-          min_width = 40,
-          height = nil,
-          max_height = 0.9,
-          min_height = 10,
-        },
-
         -- see :help dressing_get_config
         get_config = nil,
       },
