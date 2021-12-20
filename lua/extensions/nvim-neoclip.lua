51a71298f8b2732fa124f926c2af1f0d1d1f9ac0
Cenk Kılıç <cenk@kilic.dev>
2021-12-20 16:29:41 +0100

    fix: update bindings

--- lua/extensions/nvim-neoclip.lua
+++ lua/extensions/nvim-neoclip.lua
@@ -14,8 +14,10 @@ function M.config()
       content_spec_column = false,
       on_paste = { set_reg = false },
       keys = {
-        i = { select = "<cr>", paste = "<c-p>", paste_behind = "<c-k>" },
-        n = { select = "<cr>", paste = "p", paste_behind = "P" },
+        telescope = {
+          i = { select = "<cr>", paste = "<c-p>", paste_behind = "<c-k>" },
+          n = { select = "<cr>", paste = "p", paste_behind = "P" },
+        },
       },
     },
   }
