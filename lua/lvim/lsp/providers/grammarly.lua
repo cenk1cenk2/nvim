4d0fc86d63ab9f7694430736f8c87eaf027dd267
Cenk Kılıç <cenk@kilic.dev>
2022-01-11 15:15:13 +0100

    fix: adjust grammarly options

---
+++ lua/lvim/lsp/providers/grammarly.lua
@@ -0,0 +1,13 @@
+local opts = {
+  settings = {
+    grammarly = {
+      hideUnavailablePremiumAlerts = true,
+      audience = "knowledgeable",
+      dialect = "american",
+      domain = "general",
+      style = "neutral",
+    },
+  },
+}
+
+return opts
