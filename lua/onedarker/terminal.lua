8c65f048dd1da650f3629c49968128225c751846
Cenk Kılıç <cenk@kilic.dev>
2022-01-23 19:54:51 +0100

    WIP: fix some coloring in terminal

--- lua/onedarker/terminal.lua
+++ lua/onedarker/terminal.lua
@@ -3,22 +3,22 @@ local M = {}
 local c = require "onedarker.colors"
 
 function M.setup()
-  vim.g.terminal_color_0 = c.grey
+  vim.g.terminal_color_0 = c.black
   vim.g.terminal_color_1 = c.red
   vim.g.terminal_color_2 = c.green
   vim.g.terminal_color_3 = c.yellow
   vim.g.terminal_color_4 = c.blue
-  vim.g.terminal_color_5 = c.purple
+  vim.g.terminal_color_5 = c.magenta
   vim.g.terminal_color_6 = c.cyan
   vim.g.terminal_color_7 = c.fg
-  vim.g.terminal_color_8 = c.dark_grey
-  vim.g.terminal_color_9 = c.dark_red
-  vim.g.terminal_color_10 = c.dark_green
-  vim.g.terminal_color_11 = c.dark_yellow
-  vim.g.terminal_color_12 = c.dark_blue
-  vim.g.terminal_color_13 = c.dark_purple
-  vim.g.terminal_color_14 = c.dark_cyan
-  vim.g.terminal_color_15 = c.bg3
+  vim.g.terminal_color_8 = c.bright_grey
+  vim.g.terminal_color_9 = c.bright_red
+  vim.g.terminal_color_10 = c.bright_green
+  vim.g.terminal_color_11 = c.bright_yellow
+  vim.g.terminal_color_12 = c.bright_blue
+  vim.g.terminal_color_13 = c.bright_magenta
+  vim.g.terminal_color_14 = c.bright_cyan
+  vim.g.terminal_color_15 = c.white
 end
 
 return M
