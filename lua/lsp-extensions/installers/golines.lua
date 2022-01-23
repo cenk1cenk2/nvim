c1be794280f585152fd6a8e1b639c022d3d80877
Cenk Kılıç <cenk@kilic.dev>
2022-01-23 16:19:20 +0100

    WIP: update install process

--- lua/lsp-extensions/installers/golines.lua
+++ lua/lsp-extensions/installers/golines.lua
@@ -14,6 +14,6 @@ local root_dir = server.get_server_root_path(server_name)
 servers.register(server.Server:new {
   name = server_name,
   root_dir = root_dir,
-  installer = installers.pipe { go.package { "github.com/segmentio/golines" } },
+  installer = installers.pipe { go.package "github.com/segmentio/golines" },
   default_options = { cmd = { helpers.go_executable(root_dir, server_name) }, cmd_env = {} },
 })
