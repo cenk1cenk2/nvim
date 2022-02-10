8080f79640402f367c96bdcd77cca1f7d3244cc4
Cenk Kılıç <cenk@kilic.dev>
2022-02-10 23:21:30 +0100

    disable remote for now

--- lua/lvim/core/terminal.lua
+++ lua/lvim/core/terminal.lua
@@ -68,8 +68,8 @@ M.setup = function()
   vim.cmd [[
   if has('nvim') && executable('nvr')
     let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-    let $EDITOR = "nvr -cc vsplit --remote-wait +'set bufhidden=wipe'"
-    let $VISUAL = "nvr -cc vsplit --remote-wait +'set bufhidden=wipe'"
+    " let $EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+    " let $VISUAL = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
   endif
   ]]
 
