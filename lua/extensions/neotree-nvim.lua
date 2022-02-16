5856d92f6b8110fbbcff05d1e1b798eac7430c08
Cenk Kılıç <cenk@kilic.dev>
2022-02-16 14:14:45 +0100

    show git status

--- lua/extensions/neotree-nvim.lua
+++ lua/extensions/neotree-nvim.lua
@@ -9,7 +9,7 @@ function M.config()
     setup = {
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
       popup_border_style = "rounded",
-      enable_git_status = false,
+      enable_git_status = true,
       enable_diagnostics = true,
       default_component_configs = {
         indent = {
