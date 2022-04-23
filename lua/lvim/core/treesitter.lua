26457249047fb6862baeb34f8513cb46aa4d3a29
Cenk Kılıç <cenk@kilic.dev>
2022-04-24 00:32:58 +0200

    feat: add treesitter selection

--- lua/lvim/core/treesitter.lua
+++ lua/lvim/core/treesitter.lua
@@ -58,6 +58,15 @@ M.config = function()
     refactor = { highlight_current_scope = { enable = false } },
     autotag = { enable = true },
     context_commentstring = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        scope_incremental = "<CR>",
+        node_incremental = "<TAB>",
+        node_decremental = "<S-TAB>",
+      },
+    },
   }
 end
 
