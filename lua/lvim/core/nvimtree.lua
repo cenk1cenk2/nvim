5fb4be9bc6c2aa59658363ade5918f0a75a84afd
Cenk Kılıç <cenk@kilic.dev>
2022-04-24 13:58:56 +0200

    fix: update configuration for nvim tree

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -5,95 +5,10 @@ function M.config()
   lvim.builtin.nvimtree = {
     active = true,
     on_config_done = nil,
-    setup = {
-      -- disables netrw completely
-      disable_netrw = true,
-      -- hijack netrw window on startup
-      hijack_netrw = true,
-      -- open the tree when running this setup function
-      open_on_setup = false,
-      ignore_buffer_on_setup = false,
-      ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-      },
-      auto_reload_on_write = true,
-      hijack_unnamed_buffer_when_opening = false,
-      hijack_directories = {
-        enable = true,
-        auto_open = true,
-      },
-      update_to_buf_dir = {
-        enable = false,
-        auto_open = true,
-      },
-      -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-      update_focused_file = {
-        -- enables the feature
-        enable = false,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
-        update_cwd = false,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {},
-      },
-      -- configuration options for the system open command (`s` in the tree by default)
-      system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd = nil,
-        -- the command arguments as a list
-        args = {},
-      },
-      diagnostics = { enable = true, icons = { hint = "", info = "", warning = "", error = "" } },
-      git = {
-        enable = true,
-        ignore = true,
-        timeout = 200,
-      },
-      view = {
-        -- width of the window, can be either a number (columns) or a string in `%`
-        width = 40,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = "left",
-        -- if true the tree will resize itself after opening a file
-        auto_resize = true,
-        number = false,
-        relativenumber = false,
-        mappings = {
-          -- custom only false will merge the list with the default mappings
-          -- if true, it will only use your list to set the mappings
-          custom_only = false,
-          -- list of mappings to set on the tree manually
-        },
-        signcolumn = "yes",
-      },
-      window_picker_exclude = {
-        filetype = { "notify", "spectre_panel", "Outline", "packer", "qf", "lsp_floating_window" },
-        buftype = { "terminal" },
-      },
-      filters = {
-        dotfiles = false,
-        -- custom = { "node_modules", ".cache" },
-      },
-      trash = {
-        cmd = "trash",
-        require_confirm = true,
-      },
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-      },
-      special_files = { ["package.json"] = 1, ["README.md"] = 1, ["node_modules"] = 1 },
+    vars = {
+      special_files = { ["package.json"] = true, ["README.md"] = true, ["node_modules"] = true },
       show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1, tree_width = 40 },
-      quit_on_open = 0,
-      git_hl = 1,
-      disable_window_picker = 0,
-      root_folder_modifier = ":t",
-      allow_resize = 1,
-      auto_ignore_ft = { "startify", "dashboard" },
+
       icons = {
         default = "",
         symlink = "",
@@ -116,22 +31,116 @@ function M.config()
           symlink = "",
           symlink_open = "",
         },
-        open_file = {
-          resize_window = true,
-          quit_on_open = false,
+      },
+    },
+    setup = {
+      -- disables netrw completely
+      disable_netrw = true,
+      -- hijack netrw window on startup
+      hijack_netrw = true,
+      -- open the tree when running this setup function
+      open_on_setup = false,
+      ignore_buffer_on_setup = false,
+      ignore_ft_on_setup = {
+        "startify",
+        "dashboard",
+        "alpha",
+      },
+      auto_reload_on_write = true,
+      hijack_unnamed_buffer_when_opening = false,
+      hijack_directories = {
+        enable = true,
+        auto_open = true,
+      },
+      -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+      update_focused_file = {
+        -- enables the feature
+        enable = false,
+        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+        -- only relevant when `update_focused_file.enable` is true
+        update_cwd = false,
+        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+        ignore_list = {},
+      },
+      -- configuration options for the system open command (`s` in the tree by default)
+      system_open = {
+        -- the command to run this, leaving nil should work in most cases
+        cmd = nil,
+        -- the command arguments as a list
+        args = {},
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
         },
-        actions = {
-          change_dir = {
-            global = false,
-          },
-          open_file = {
-            quit_on_open = false,
-          },
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 200,
+      },
+      view = {
+        -- width of the window, can be either a number (columns) or a string in `%`
+        width = 40,
+        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+        side = "left",
+        -- if true the tree will resize itself after opening a file
+        number = false,
+        relativenumber = false,
+        mappings = {
+          -- custom only false will merge the list with the default mappings
+          -- if true, it will only use your list to set the mappings
+          custom_only = false,
+          -- list of mappings to set on the tree manually
+        },
+        signcolumn = "yes",
+      },
+      filters = {
+        dotfiles = false,
+        -- custom = { "node_modules", ".cache" },
+      },
+      trash = {
+        cmd = "trash",
+        require_confirm = true,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+      },
+      actions = {
+        change_dir = {
+          global = false,
+        },
+        open_file = {
+          quit_on_open = false,
+          resize_window = true,
           window_picker = {
             enable = false,
             chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-            exclude = {},
+            exclude = {
+              filetype = { "notify", "spectre_panel", "Outline", "packer", "qf", "lsp_floating_window" },
+              buftype = { "terminal" },
+            },
           },
+        },
+      },
+      log = {
+        enable = false,
+        truncate = false,
+        types = {
+          all = false,
+          config = false,
+          copy_paste = false,
+          diagnostics = false,
+          git = false,
+          profile = false,
         },
       },
     },
@@ -149,35 +158,8 @@ function M.setup()
   end
 
   local table_utils = require "lvim.utils.table"
-  for opt, val in pairs(lvim.builtin.nvimtree.setup) do
-    if
-      not table_utils.contains({
-        "disable_netrw",
-        "hijack_netrw",
-        "open_on_setup",
-        "ignore_ft_on_setup",
-        "auto_close",
-        "open_on_tab",
-        "hijack_cursor",
-        "update_cwd",
-        "update_to_buf_dir",
-        "diagnostics",
-        "update_focused_file",
-        "system_open",
-        "filters",
-        "git",
-        "view",
-        "trash",
-        "disable_window_picker",
-        "quit_on_open",
-        "window_picker_exclude",
-        "renderer",
-      }, function(entry)
-        return opt == entry
-      end)
-    then
-      vim.g["nvim_tree_" .. opt] = val
-    end
+  for opt, val in pairs(lvim.builtin.nvimtree.vars) do
+    vim.g["nvim_tree_" .. opt] = val
   end
 
   -- Implicitly update nvim-tree when project module is active
