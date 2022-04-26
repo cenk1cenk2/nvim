98d3c410b73004bed46a808ce180979f8f9e59bd
Cenk Kılıç <cenk@kilic.dev>
2022-04-26 11:05:52 +0200

    fix bootstrapping issue

--- lua/lvim/bootstrap.lua
+++ lua/lvim/bootstrap.lua
@@ -32,32 +32,20 @@ end
 ---Get the full path to `$LUNARVIM_RUNTIME_DIR`
 ---@return string
 function _G.get_runtime_dir()
-  local lvim_runtime_dir = os.getenv "LUNARVIM_RUNTIME_DIR"
-  if not lvim_runtime_dir then
-    -- when nvim is used directly
-    return vim.call("stdpath", "data")
-  end
-  return lvim_runtime_dir
+  -- when nvim is used directly
+  return vim.fn.stdpath "data"
 end
 
 ---Get the full path to `$LUNARVIM_CONFIG_DIR`
 ---@return string
 function _G.get_config_dir()
-  local lvim_config_dir = os.getenv "LUNARVIM_CONFIG_DIR"
-  if not lvim_config_dir then
-    return vim.call("stdpath", "config")
-  end
-  return lvim_config_dir
+  return vim.fn.stdpath "config"
 end
 
 ---Get the full path to `$LUNARVIM_CACHE_DIR`
 ---@return string
 function _G.get_cache_dir()
-  local lvim_cache_dir = os.getenv "LUNARVIM_CACHE_DIR"
-  if not lvim_cache_dir then
-    return vim.call("stdpath", "config")
-  end
-  return lvim_cache_dir
+  return vim.fn.stdpath "cache"
 end
 
 ---Initialize the `&runtimepath` variables and prepare for startup
@@ -69,18 +57,6 @@ function M:init(base_dir)
   self.pack_dir = join_paths(self.runtime_dir, "site", "pack")
   self.packer_install_dir = join_paths(self.runtime_dir, "site", "pack", "packer", "start", "packer.nvim")
   self.packer_cache_path = join_paths(self.config_dir, "plugin", "packer_compiled.lua")
-
-  ---@meta overridden to use LUNARVIM_x_DIR instead, since a lot of plugins call this function interally
-  vim.fn.stdpath = function(what)
-    if what == "cache" then
-      return _G.get_cache_dir()
-    elseif what == "data" then
-      return _G.get_runtime_dir()
-    elseif what == "config" then
-      return _G.get_config_dir()
-    end
-    return vim.call("stdpath", what)
-  end
 
   ---Get the full path to LunarVim's base directory
   ---@return string
