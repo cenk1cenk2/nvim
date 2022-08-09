6f61eaed1ae94756ddafc3cdaaadcdf4a7e60c9d
Cenk Kılıç <cenk@kilic.dev>
2022-08-09 21:45:17 +0200

    fix: update keymaps

--- lua/extensions/nvim-surround.lua
+++ lua/extensions/nvim-surround.lua
@@ -6,7 +6,20 @@ function M.config()
   lvim.extensions[extension_name] = {
     active = true,
     on_config_done = nil,
-    setup = {},
+    setup = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "s",
+        visual_line = "gs",
+        delete = "ds",
+        change = "cs",
+      },
+    },
   }
 end
 
