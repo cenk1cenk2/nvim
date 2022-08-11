a0b7adc6ddf877fd1ebefec3963dd262b124b29c
Cenk Kılıç <cenk@kilic.dev>
2022-08-11 18:24:33 +0200

    fix: change settings for plugin

--- lua/extensions/cool-substitute-nvim.lua
+++ lua/extensions/cool-substitute-nvim.lua
@@ -7,20 +7,18 @@ function M.config()
     active = true,
     on_config_done = nil,
     setup = {
-      {
-        setup_keybindings = true,
-        mappings = {
-          start = "gm", -- Mark word / region
-          start_and_edit = "gM", -- Mark word / region and also edit
-          start_and_edit_word = "g!M", -- Mark word / region and also edit.  Edit only full word.
-          start_word = "g!m", -- Mark word / region. Edit only full word
-          apply_substitute_and_next = "M", -- Start substitution / Go to next substitution
-          apply_substitute_and_prev = "<C-b>", -- same as M but backwards
-          apply_substitute_all = "ga", -- Substitute all
-        },
-        reg_char = "o", -- letter to save macro (Dont use number or uppercase here)
-        mark_char = "t", -- mark the position at start of macro
+      setup_keybindings = true,
+      mappings = {
+        start = "gm", -- Mark word / region
+        start_and_edit = "gM", -- Mark word / region and also edit
+        start_and_edit_word = "g!M", -- Mark word / region and also edit.  Edit only full word.
+        start_word = "g!m", -- Mark word / region. Edit only full word
+        apply_substitute_and_next = "M", -- Start substitution / Go to next substitution
+        apply_substitute_and_prev = "<C-b>", -- same as M but backwards
+        apply_substitute_all = "ga", -- Substitute all
       },
+      reg_char = "o", -- letter to save macro (Dont use number or uppercase here)
+      mark_char = "t", -- mark the position at start of macro
     },
   }
 end
