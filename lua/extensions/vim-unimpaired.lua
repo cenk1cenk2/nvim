1a13ab5572b97504bff82569febde59795692805
Cenk Kılıç <cenk@kilic.dev>
2022-09-01 22:08:16 +0200

    fix: disable unimpaired

--- lua/extensions/vim-unimpaired.lua
+++ lua/extensions/vim-unimpaired.lua
@@ -4,7 +4,7 @@ local extension_name = "vim_unimpaired"
 
 function M.config()
   lvim.extensions[extension_name] = {
-    active = true,
+    active = false,
     on_config_done = nil,
     setup = {},
     keymaps = {},
