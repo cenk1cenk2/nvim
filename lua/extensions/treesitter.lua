95cf55dd2eae41e47aefc914d7924a34cdc83f4a
Cenk Kılıç <cenk@kilic.dev>
2022-09-21 22:08:46 +0200

    fix: html highlighting

--- lua/extensions/treesitter.lua
+++ lua/extensions/treesitter.lua
@@ -74,7 +74,7 @@ function M.config()
       },
       rainbow = {
         enable = true,
-        extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+        extended_mode = false, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
         max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
         colors = { "#ffd700", "#8bb9dd" },
         disable = lvim.disabled_filetypes,
