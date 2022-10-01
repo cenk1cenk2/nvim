2402b169a913a728ea9f0690293fe7bfedcf05e3
Cenk Kılıç <cenk@kilic.dev>
2022-10-01 14:24:46 +0200

    fix: update rebuild terminal

--- lua/modules/nvim.lua
+++ lua/modules/nvim.lua
@@ -1,15 +1,10 @@
 local M = {}
 
 function M.rebuild_latest_neovim()
-  local term_opts = vim.tbl_deep_extend("force", require("extensions.toggleterm-nvim").current_setup(), {
+  local term_opts = require("extensions.toggleterm-nvim").generate_defaults_float_terminal {
     cmd = join_paths(get_config_dir(), "/utils/install-latest-neovim.sh"),
-    open_mapping = lvim.log.viewer.layout_config.open_mapping,
-    direction = lvim.log.viewer.layout_config.direction,
-    -- TODO: this might not be working as expected
-    size = lvim.log.viewer.layout_config.size,
-    float_opts = lvim.log.viewer.layout_config.float_opts,
     close_on_exit = false,
-  })
+  }
 
   local Terminal = require("toggleterm.terminal").Terminal
   local log_view = Terminal:new(term_opts)
