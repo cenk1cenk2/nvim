82c2fe7e102b43c9fbbca5dce8c3f3c0cd9610a8
Cenk Kılıç <cenk@kilic.dev>
2022-10-23 19:43:42 +0200

    fix: update ocnfiguration for outline

--- lua/extensions/symbols-outline.lua
+++ lua/extensions/symbols-outline.lua
@@ -15,13 +15,17 @@ function M.config()
         disable = not config.active,
       }
     end,
+    configure = function()
+      table.insert(lvim.disabled_filetypes, "Outline")
+    end,
     setup = {
       highlight_hovered_item = true,
       show_guides = true,
       auto_preview = true,
       preview_bg_highlight = "NormalFloat",
       position = "right",
-      width = 50,
+      relative_width = true,
+      width = 40,
       keymaps = {
         close = "q",
         goto_location = "<Cr>",
