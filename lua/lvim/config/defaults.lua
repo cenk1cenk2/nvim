26c66d1e49d3ad7a76863cebf943894e4221a77e
Cenk Kılıç <cenk@kilic.dev>
2022-12-15 20:34:42 +0100

    fix: change to ignore retrail

--- lua/lvim/config/defaults.lua
+++ lua/lvim/config/defaults.lua
@@ -58,7 +58,6 @@ lvim = {
 
   -- add disabled filetypes function was a bit unreliable due to cached loading of this object
   disabled_filetypes = {
-    "",
     "neo-tree",
     "terminal",
     "packer",
