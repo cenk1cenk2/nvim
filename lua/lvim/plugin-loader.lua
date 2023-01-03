f2791962ee8aa8587280d70fba7a3490d4eb427c
Cenk Kılıç <cenk@kilic.dev>
2023-01-03 13:08:18 +0100

    fix: dont notify on updates

--- lua/lvim/plugin-loader.lua
+++ lua/lvim/plugin-loader.lua
@@ -72,7 +72,7 @@ function M.load()
       -- automatically check for plugin updates
       enabled = true,
       concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-      notify = true, -- get a notification when new updates are found
+      notify = false, -- get a notification when new updates are found
       frequency = 3600, -- check for updates every hour
     },
     change_detection = {
