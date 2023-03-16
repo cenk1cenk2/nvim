07f741524f92767dd727c90fc2053d629e5c6b10
Cenk Kılıç <cenk@kilic.dev>
2023-03-16 13:28:15 +0100

    fix: toggleterm seems to remember last mode can start in insert

--- lua/extensions/toggleterm-nvim.lua
+++ lua/extensions/toggleterm-nvim.lua
@@ -52,7 +52,7 @@ function M.config()
       shade_filetypes = {},
       shade_terminals = true,
       shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      start_in_insert = false,
+      start_in_insert = true,
       insert_mappings = false, -- whether or not the open mapping applies in insert mode
       persist_size = false,
       -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
