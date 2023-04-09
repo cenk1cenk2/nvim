de473700a3d3c2cc53c356d37c9cd5307d18bb18
Cenk Kılıç <cenk@kilic.dev>
2023-04-09 13:32:26 +0200

    fix: enable plugin again

--- lua/extensions/stickybuf-nvim.lua
+++ lua/extensions/stickybuf-nvim.lua
@@ -4,7 +4,7 @@ local M = {}
 local extension_name = "stickybuf_nvim"
 
 function M.config()
-  require("utils.setup").define_extension(extension_name, false, {
+  require("utils.setup").define_extension(extension_name, true, {
     plugin = function()
       return {
         "stevearc/stickybuf.nvim",
