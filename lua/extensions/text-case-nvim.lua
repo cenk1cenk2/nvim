30a7415ab9f403bad44bac37203b6ed52f20905c
Cenk Kılıç <cenk@kilic.dev>
2023-06-02 15:27:55 +0200

    fix: update casing details

--- lua/extensions/text-case-nvim.lua
+++ lua/extensions/text-case-nvim.lua
@@ -19,83 +19,81 @@ function M.config()
     -- subs command comes from here!
     -- require("textcase").setup(config.setup)
     -- end,
-    keymaps = function()
-      return {
-        {
-          { "n", "v" },
+    keymaps = {
+      {
+        { "v" },
 
-          ["gsu"] = {
-            function()
-              return require("textcase").operator("to_upper_case")
-            end,
-            { desc = "to upper case" },
-          },
-
-          ["gsl"] = {
-            function()
-              return require("textcase").operator("to_lower_case")
-            end,
-            { desc = "to lower case" },
-          },
-
-          ["gss"] = {
-            function()
-              return require("textcase").operator("to_snake_case")
-            end,
-            { desc = "to snake case" },
-          },
-
-          ["gsd"] = {
-            function()
-              return require("textcase").operator("to_dash_case")
-            end,
-            { desc = "to dash case" },
-          },
-
-          ["gsC"] = {
-            function()
-              return require("textcase").operator("to_constant_case")
-            end,
-            { desc = "to constant case" },
-          },
-
-          ["gsP"] = {
-            function()
-              return require("textcase").operator("to_phrase_case")
-            end,
-            { desc = "to phrase case" },
-          },
-
-          ["gsc"] = {
-            function()
-              return require("textcase").operator("to_camel_case")
-            end,
-            { desc = "to camel case" },
-          },
-
-          ["gsp"] = {
-            function()
-              return require("textcase").operator("to_pascal_case")
-            end,
-            { desc = "to pascal case" },
-          },
-
-          ["gst"] = {
-            function()
-              return require("textcase").operator("to_title_case")
-            end,
-            { desc = "to title case" },
-          },
-
-          ["gsD"] = {
-            function()
-              return require("textcase").operator("to_path_case")
-            end,
-            { desc = "to path case" },
-          },
+        ["gsu"] = {
+          function()
+            return require("textcase").operator("to_upper_case")
+          end,
+          { desc = "to upper case" },
         },
-      }
-    end,
+
+        ["gsl"] = {
+          function()
+            return require("textcase").operator("to_lower_case")
+          end,
+          { desc = "to lower case" },
+        },
+
+        ["gss"] = {
+          function()
+            return require("textcase").operator("to_snake_case")
+          end,
+          { desc = "to snake case" },
+        },
+
+        ["gsd"] = {
+          function()
+            return require("textcase").operator("to_dash_case")
+          end,
+          { desc = "to dash case" },
+        },
+
+        ["gsC"] = {
+          function()
+            return require("textcase").operator("to_constant_case")
+          end,
+          { desc = "to constant case" },
+        },
+
+        ["gsP"] = {
+          function()
+            return require("textcase").operator("to_phrase_case")
+          end,
+          { desc = "to phrase case" },
+        },
+
+        ["gsc"] = {
+          function()
+            return require("textcase").operator("to_camel_case")
+          end,
+          { desc = "to camel case" },
+        },
+
+        ["gsp"] = {
+          function()
+            return require("textcase").operator("to_pascal_case")
+          end,
+          { desc = "to pascal case" },
+        },
+
+        ["gst"] = {
+          function()
+            return require("textcase").operator("to_title_case")
+          end,
+          { desc = "to title case" },
+        },
+
+        ["gsD"] = {
+          function()
+            return require("textcase").operator("to_path_case")
+          end,
+          { desc = "to path case" },
+        },
+      },
+    },
   })
 end
 
