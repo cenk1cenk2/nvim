b1f6d00563732f865a90d29753b8b7ce4afa52aa
Cenk Kılıç <cenk@kilic.dev>
2023-06-26 16:18:20 +0200

    fix: disable search

--- lua/extensions/flash-nvim.lua
+++ lua/extensions/flash-nvim.lua
@@ -126,7 +126,7 @@ function M.config()
           -- options used when flash is activated through
           -- a regular search with `/` or `?`
           search = {
-            enabled = true, -- enable flash for search
+            enabled = false, -- enable flash for search
             highlight = { backdrop = false },
             jump = { history = true, register = true, nohlsearch = true },
             search = {
