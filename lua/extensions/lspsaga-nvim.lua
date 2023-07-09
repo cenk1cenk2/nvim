78855f407ba553d4d0eb627abde40ab38ea6dc62
Cenk Kılıç <cenk@kilic.dev>
2023-07-09 19:13:41 +0200

    fix: update configuration

--- lua/extensions/lspsaga-nvim.lua
+++ lua/extensions/lspsaga-nvim.lua
@@ -1,4 +1,4 @@
--- https://github.com/glepnir/lspsaga.nvim
+-- https://github.com/nvimdev/lspsaga.nvim
 local M = {}
 
 local extension_name = "lspsaga_nvim"
@@ -7,7 +7,7 @@ function M.config()
   require("utils.setup").define_extension(extension_name, true, {
     plugin = function()
       return {
-        "glepnir/lspsaga.nvim",
+        "nvimdev/lspsaga.nvim",
         event = "LspAttach",
       }
     end,
@@ -21,92 +21,94 @@ function M.config()
         "sagafinder",
       })
     end,
-    setup = {
-      lightbulb = {
-        enable = false,
-        enable_in_insert = false,
-        sign = true,
-        update_time = 1000,
-        virtual_text = false,
-      },
-      code_action = {
-        num_shortcut = true,
-        show_server_name = true,
-        extend_gitsigns = false,
-        keys = {
-          -- string | table type
+    setup = function()
+      return {
+        lightbulb = {
+          enable = false,
+          enable_in_insert = false,
+          sign = true,
+          update_time = 1000,
+          virtual_text = false,
+        },
+        code_action = {
+          num_shortcut = true,
+          show_server_name = true,
+          extend_gitsigns = false,
+          keys = {
+            -- string | table type
+            quit = "q",
+            exec = "<CR>",
+          },
+        },
+        finder = {
+          open = "<CR>",
+          vsplit = "s",
+          split = "h",
           quit = "q",
+          scroll_down = "<C-f>",
+          scroll_up = "<C-b>", -- quit can be a table
+        },
+        diagnostic = {
+          -- maybe disable this later on
+          -- https://www.reddit.com/r/neovim/comments/11o68vz/text_appearing_on_the_corner_of_the_screen/?utm_source=share&utm_medium=android_app&utm_name=androidcss&utm_term=2&utm_content=share_button
+          on_insert = false,
+          on_insert_follow = true,
+          insert_winblend = 0,
+          twice_into = true,
+          show_code_action = false,
+          show_source = true,
+          keys = {
+            exec_action = "o",
+            quit = "q",
+          },
+        },
+        rename = {
+          quit = "<C-c>",
           exec = "<CR>",
+          in_select = false,
         },
-      },
-      finder = {
-        open = "<CR>",
-        vsplit = "s",
-        split = "h",
-        quit = "q",
-        scroll_down = "<C-f>",
-        scroll_up = "<C-b>", -- quit can be a table
-      },
-      diagnostic = {
-        -- maybe disable this later on
-        -- https://www.reddit.com/r/neovim/comments/11o68vz/text_appearing_on_the_corner_of_the_screen/?utm_source=share&utm_medium=android_app&utm_name=androidcss&utm_term=2&utm_content=share_button
-        on_insert = false,
-        on_insert_follow = true,
-        insert_winblend = 0,
-        twice_into = true,
-        show_code_action = false,
-        show_source = true,
-        keys = {
-          exec_action = "o",
-          quit = "q",
+        -- show symbols in winbar must nightly
+        symbol_in_winbar = {
+          in_custom = false,
+          enable = true,
+          separator = (" %s "):format(lvim.ui.icons.ui.ChevronShortRight),
+          show_file = true,
+          folder_level = 1,
+          respect_root = true,
         },
-      },
-      rename = {
-        quit = "<C-c>",
-        exec = "<CR>",
-        in_select = false,
-      },
-      -- show symbols in winbar must nightly
-      symbol_in_winbar = {
-        in_custom = false,
-        enable = true,
-        separator = (" %s "):format(lvim.ui.icons.ui.ChevronShortRight),
-        show_file = true,
-        folder_level = 1,
-        respect_root = true,
-      },
-      ui = {
-        -- currently only round theme
-        theme = "round",
-        -- border type can be single,double,rounded,solid,shadow.
-        border = lvim.ui.border,
-        winblend = 0,
-        expand = lvim.ui.icons.ui.ChevronShortLeft,
-        collaspe = lvim.ui.icons.ui.ChevronShortDown,
-        preview = lvim.ui.icons.ui.FindFile,
-        code_action = "💡",
-        diagnostic = lvim.ui.icons.ui.Bug,
-        incoming = " ",
-        outgoing = " ",
-        colors = {
-          --float window normal bakcground color
-          normal_bg = lvim.ui.colors.bg[200],
-          --title background color
-          title_bg = lvim.ui.colors.yellow[300],
-          red = lvim.ui.colors.red[600],
-          magenta = lvim.ui.colors.magenta[600],
-          orange = lvim.ui.colors.orange[600],
-          yellow = lvim.ui.colors.yellow[600],
-          green = lvim.ui.colors.green[600],
-          cyan = lvim.ui.colors.cyan[600],
-          blue = lvim.ui.colors.blue[600],
-          purple = lvim.ui.colors.purple[600],
-          white = lvim.ui.colors.white,
-          black = lvim.ui.colors.black,
+        ui = {
+          -- currently only round theme
+          theme = "round",
+          -- border type can be single,double,rounded,solid,shadow.
+          border = lvim.ui.border,
+          winblend = 0,
+          expand = lvim.ui.icons.ui.ChevronShortLeft,
+          collaspe = lvim.ui.icons.ui.ChevronShortDown,
+          preview = lvim.ui.icons.ui.FindFile,
+          code_action = "💡",
+          diagnostic = lvim.ui.icons.ui.Bug,
+          incoming = " ",
+          outgoing = " ",
+          colors = {
+            --float window normal bakcground color
+            normal_bg = lvim.ui.colors.bg[200],
+            --title background color
+            title_bg = lvim.ui.colors.yellow[300],
+            red = lvim.ui.colors.red[600],
+            magenta = lvim.ui.colors.magenta[600],
+            orange = lvim.ui.colors.orange[600],
+            yellow = lvim.ui.colors.yellow[600],
+            green = lvim.ui.colors.green[600],
+            cyan = lvim.ui.colors.cyan[600],
+            blue = lvim.ui.colors.blue[600],
+            purple = lvim.ui.colors.purple[600],
+            white = lvim.ui.colors.white,
+            black = lvim.ui.colors.black,
+          },
+          kind = {},
         },
-        kind = {},
-      },
-    },
+      }
+    end,
     on_setup = function(config)
       require("lspsaga").setup(config.setup)
     end,
