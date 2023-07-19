f01fc8b191054086aa90b1abcd2b2197b4f479f4
Cenk Kılıç <cenk@kilic.dev>
2023-07-19 20:01:24 +0200

    fix: go back to tab

--- lua/extensions/flatten-nvim.lua
+++ lua/extensions/flatten-nvim.lua
@@ -67,7 +67,7 @@ function M.config()
           -- func(new_bufs, argv) -> only open the files, allowing you to handle window opening yourself.
           -- Argument is an array of buffer numbers representing the newly opened files.
           -- open = "alternate",
-          open = "alternate",
+          open = "tab",
           -- Affects which file gets focused when opening multiple at once
           -- Options:
           -- "first"        -> open first file of new files (default)
