06f1d2f5e4cb978569f5567323a85611809bbbb6
Cenk Kılıç <cenk@kilic.dev>
2023-07-23 15:08:30 +0200

    fix: update keybindings

--- lua/extensions/nvim-navbuddy.lua
+++ lua/extensions/nvim-navbuddy.lua
@@ -45,41 +45,41 @@ function M.config()
         },
         icons = lvim.ui.icons.kind,
         mappings = {
-          ["<esc>"] = actions.close, -- Close and cursor to original location
-          ["q"] = actions.close,
+          ["<esc>"] = actions.close(), -- Close and cursor to original location
+          ["q"] = actions.close(),
 
-          ["j"] = actions.next_sibling, -- down
-          ["k"] = actions.previous_sibling, -- up
+          ["j"] = actions.next_sibling(), -- down
+          ["k"] = actions.previous_sibling(), -- up
 
-          ["h"] = actions.parent, -- Move to left panel
-          ["l"] = actions.children, -- Move to right panel
+          ["h"] = actions.parent(), -- Move to left panel
+          ["l"] = actions.children(), -- Move to right panel
 
-          ["v"] = actions.visual_name, -- Visual selection of name
-          ["V"] = actions.visual_scope, -- Visual selection of scope
+          ["v"] = actions.visual_name(), -- Visual selection of name
+          ["V"] = actions.visual_scope(), -- Visual selection of scope
 
-          ["y"] = actions.yank_name, -- Yank the name to system clipboard "+
-          ["Y"] = actions.yank_scope, -- Yank the scope to system clipboard "+
+          ["y"] = actions.yank_name(), -- Yank the name to system clipboard "+
+          ["Y"] = actions.yank_scope(), -- Yank the scope to system clipboard "+
 
-          ["i"] = actions.insert_name, -- Insert at start of name
-          ["I"] = actions.insert_scope, -- Insert at start of scope
+          ["i"] = actions.insert_name(), -- Insert at start of name
+          ["I"] = actions.insert_scope(), -- Insert at start of scope
 
-          ["a"] = actions.append_name, -- Insert at end of name
-          ["A"] = actions.append_scope, -- Insert at end of scope
+          ["a"] = actions.append_name(), -- Insert at end of name
+          ["A"] = actions.append_scope(), -- Insert at end of scope
 
-          ["r"] = actions.rename, -- Rename currently focused symbol
+          ["r"] = actions.rename(), -- Rename currently focused symbol
 
-          ["D"] = actions.delete, -- Delete scope
+          ["D"] = actions.delete(), -- Delete scope
 
-          ["f"] = actions.fold_create, -- Create fold of current scope
-          ["F"] = actions.fold_delete, -- Delete fold of current scope
+          ["f"] = actions.fold_create(), -- Create fold of current scope
+          ["F"] = actions.fold_delete(), -- Delete fold of current scope
 
-          ["C"] = actions.comment, -- Comment out current scope
+          ["C"] = actions.comment(), -- Comment out current scope
 
-          ["<enter>"] = actions.select, -- Goto selected symbol
-          ["o"] = actions.select,
+          ["<enter>"] = actions.select(), -- Goto selected symbol
+          ["o"] = actions.select(),
 
-          ["J"] = actions.move_down, -- Move focused node down
-          ["K"] = actions.move_up, -- Move focused node up
+          ["J"] = actions.move_down(), -- Move focused node down
+          ["K"] = actions.move_up(), -- Move focused node up
         },
         lsp = {
           auto_attach = true, -- If set to true, you don't need to manually use attach function
