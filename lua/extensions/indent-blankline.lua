47d05bc90b28534e85f9b111e39f2627e3aea5b2
Cenk Kılıç <cenk@kilic.dev>
2023-09-28 09:51:27 +0200

    fix(extensions/indent-blankline.nvim): update plugin config

--- lua/extensions/indent-blankline.lua
+++ lua/extensions/indent-blankline.lua
@@ -13,40 +13,66 @@ function M.config()
     end,
     setup = function()
       return {
-        use_treesitter = true,
-        show_current_context = true,
-        show_first_indent_level = false,
-        filetype_exclude = lvim.disabled_filetypes,
-        enabled = true,
-        show_trailing_blankline_indent = true,
-        char = lvim.ui.icons.ui.LineLeft,
-        space_char_highlight_list = {},
-        context_patterns = {
-          "class",
-          "return",
-          "function",
-          "method",
-          "^if",
-          "^while",
-          "jsx_element",
-          "^for",
-          "^object",
-          "^table",
-          "block",
-          "arguments",
-          "if_statement",
-          "else_clause",
-          "jsx_element",
-          "jsx_self_closing_element",
-          "try_statement",
-          "catch_clause",
-          "import_statement",
-          "operation_type",
+        indent = { highlight = "IndentBlankLineChar", char = lvim.ui.icons.LineLeft },
+        whitespace = {
+          highlight = "IndentBlankLineChar",
+          remove_blankline_trail = false,
+        },
+        scope = {
+          enabled = true,
+          char = lvim.ui.icons.LineLeft,
+          show_start = false,
+          show_end = false,
+          injected_languages = true,
+          highlight = "IndentBlanklineContextChar",
+          priority = 1024,
+          include = {
+            node_type = {
+              "class",
+              "return",
+              "function",
+              "method",
+              "^if",
+              "^while",
+              "jsx_element",
+              "^for",
+              "^object",
+              "^table",
+              "block",
+              "arguments",
+              "if_statement",
+              "else_clause",
+              "jsx_element",
+              "jsx_self_closing_element",
+              "try_statement",
+              "catch_clause",
+              "import_statement",
+              "operation_type",
+            },
+          },
+          exclude = {
+            language = {},
+            node_type = {
+              ["*"] = {
+                "source_file",
+                "program",
+              },
+              lua = {
+                "chunk",
+              },
+              python = {
+                "module",
+              },
+            },
+          },
+        },
+        exclude = {
+          filetypes = lvim.disabled_filetypes,
         },
       }
     end,
     on_setup = function(config)
-      require("indent_blankline").setup(config.setup)
+      require("ibl").setup(config.setup)
     end,
     wk = function(_, categories)
       return {
