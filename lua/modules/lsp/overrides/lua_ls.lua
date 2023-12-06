d17532a04c88b4e140583c4607746e7820ecae11
Cenk Kılıç <cenk@kilic.dev>
2023-12-06 15:01:42 +0100

    fix: update lua lang server

--- lua/modules/lsp/overrides/lua_ls.lua
+++ lua/modules/lsp/overrides/lua_ls.lua
@@ -1,10 +1,17 @@
 local neodev_loaded, neodev = pcall(require, "neodev")
+local options = {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+}
 
 if neodev_loaded then
   neodev.setup({
     library = {
       vimruntime = true, -- runtime path
-      types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+      types = true,      -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
       -- plugins = true, -- installed opt or start plugins in packpath
       -- you can also specify the list of plugins to make available as a workspace library
       plugins = { "plenary.nvim" },
@@ -12,7 +19,7 @@ if neodev_loaded then
     override = nil, -- function(root_dir, options) end,
   })
 
-  return {
+  options = vim.tbl_extend("force", options, {
     settings = {
       Lua = {
         diagnostics = {
@@ -32,7 +39,7 @@ if neodev_loaded then
         },
       },
     },
-  }
+  })
 end
 
-return {}
+return options
