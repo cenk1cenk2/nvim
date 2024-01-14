90c17bbbdc31dea66d6d43005b2527f68f5d7458
Cenk Kılıç <cenk@kilic.dev>
2024-01-14 15:47:55 +0100

    fix: update notes locations

--- lua/extensions/obsidian-nvim.lua
+++ lua/extensions/obsidian-nvim.lua
@@ -37,7 +37,7 @@ function M.config()
 
         daily_notes = {
           -- Optional, if you keep daily notes in a separate directory.
-          folder = "notes/dailies",
+          folder = "calendar",
           -- Optional, if you want to change the date format for the ID of daily notes.
           date_format = "%Y-%m-%d",
           -- Optional, if you want to change the date format of the default alias of daily notes.
