b05ceff9e72e3a890cca412f4614a423cfa75949
Cenk Kılıç <cenk@kilic.dev>
2024-01-14 15:06:45 +0100

    fix: update autopairs

--- lua/extensions/nvim-autopairs.lua
+++ lua/extensions/nvim-autopairs.lua
@@ -130,9 +130,17 @@ function M.config()
     on_done = function()
       require("nvim-treesitter.configs").setup({ autopairs = { enable = true } })
 
-      pcall(function()
-        require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
-      end)
+      if package.loaded["cmp"] then
+        -- require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+
+        require("cmp").event:on("confirm_done", function(evt)
+          if evt.entry.completion_item then
+            require("nvim-autopairs.completion.cmp").on_confirm_done()(evt)
+
+            vim.api.nvim_exec_autocmds("CompleteChanged", {})
+          end
+        end)
+      end
     end,
   })
 end
