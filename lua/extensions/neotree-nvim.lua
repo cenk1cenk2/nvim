04c10768ea488999a179fa4036ac56af1352c9d4
Cenk Kılıç <cenk@kilic.dev>
2024-01-23 12:46:01 +0100

    fix: issue with typo

--- lua/extensions/neotree-nvim.lua
+++ lua/extensions/neotree-nvim.lua
@@ -255,7 +255,7 @@ function M.config()
             },
           },
           -- follow_current_file = false,
-          follow_curren_file = {
+          follow_current_file = {
             enabled = true, -- This will find and focus the file in the active buffer every time
             --               -- the current file is changed while the tree is open.
             leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
