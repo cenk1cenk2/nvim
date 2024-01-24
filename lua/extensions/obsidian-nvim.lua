02747485c30e8783335737706c5dcda2979f5eea
Cenk Kılıç <cenk@kilic.dev>
2024-01-24 15:26:29 +0100

    fix: update frontmatter

--- lua/extensions/obsidian-nvim.lua
+++ lua/extensions/obsidian-nvim.lua
@@ -47,6 +47,23 @@ function M.config()
           template = nil,
         },
 
+        note_frontmatter_func = function(note)
+          -- This is equivalent to the default frontmatter function.
+          local out = {
+            id = note.id,
+            aliases = note.aliases,
+            tags = note.tags,
+          }
+          -- `note.metadata` contains any manually added fields in the frontmatter.
+          -- So here we just make sure those fields are kept in the frontmatter.
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+          return out
+        end,
+
         -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
         completion = {
           -- Set to false to disable completion.
