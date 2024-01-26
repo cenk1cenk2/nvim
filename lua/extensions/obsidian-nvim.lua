bb9707195a26ae34c88ba9ce6e1d30d94dfeaf4a
Cenk Kılıç <cenk@kilic.dev>
2024-01-26 14:38:25 +0100

    fix: update obsidian conf

--- lua/extensions/obsidian-nvim.lua
+++ lua/extensions/obsidian-nvim.lua
@@ -57,7 +57,7 @@ function M.config()
           -- Optional, if you want to change the date format of the default alias of daily notes.
           alias_format = "%B %-d, %Y",
           -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-          template = "Templates/Daily.md",
+          template = "Daily.md",
         },
 
         disable_frontmatter = true,
