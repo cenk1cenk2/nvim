24db479cec158e2bdc0597eaa3d2c06f37ef28ab
Cenk Kılıç <cenk@kilic.dev>
2024-02-24 20:40:41 +0100

    fix: disable lsptimeout for now

--- lua/extensions/lsp-timeout-nvim.lua
+++ lua/extensions/lsp-timeout-nvim.lua
@@ -4,7 +4,7 @@ local M = {}
 local extension_name = "hinell/lsp-timeout.nvim"
 
 function M.config()
-  require("utils.setup").define_extension(extension_name, true, {
+  require("utils.setup").define_extension(extension_name, false, {
     plugin = function()
       return {
         "hinell/lsp-timeout.nvim",
