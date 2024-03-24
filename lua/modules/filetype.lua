af79f17324f862b1175cba0dfef36a128188b5e2
Cenk Kılıç <cenk@kilic.dev>
2024-03-24 16:48:19 +0100

    fix: update md conf

--- lua/modules/filetype.lua
+++ lua/modules/filetype.lua
@@ -8,6 +8,12 @@ function M.setup()
 
         return "jinja"
       end,
+      ["md"] = function()
+        vim.api.nvim_set_option_value("shiftwidth", 2, { buf = 0 })
+        vim.api.nvim_set_option_value("tabstop", 2, { buf = 0 })
+
+        return "markdown"
+      end,
       ["tf"] = "terraform",
       ["tfvars"] = "terraform",
       ["zsh"] = "sh",
