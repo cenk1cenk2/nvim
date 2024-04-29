96c3acfb50e49ff5d6f6f9b2d9b5e21b2a084ed8
Cenk Kılıç <cenk@kilic.dev>
2024-04-29 15:05:29 +0200

    fix: update diagnostic severity

--- lua/modules/lsp/overrides/typos_lsp.lua
+++ lua/modules/lsp/overrides/typos_lsp.lua
@@ -1,4 +1,15 @@
 return {
+  handlers = {
+    ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+      result.diagnostics = vim.tbl_map(function(diagnostic)
+        diagnostic.severity = vim.diagnostic.severity.HINT
+
+        return diagnostic
+      end, result.diagnostics)
+
+      return vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
+    end,
+  },
   filetypes = {
     "markdown",
     "plaintext",
