fe622473deed182001cee9fe4c375d1d631b8b23
Cenk Kılıç <cenk@kilic.dev>
2024-05-03 11:16:23 +0200

    fix: dont remove code lens refresh augroup since logic is different

--- lua/lvim/lsp/init.lua
+++ lua/lvim/lsp/init.lua
@@ -41,9 +41,9 @@ function M.common_on_exit(client, bufnr)
     Log:trace("Called lsp.on_exit")
   end
 
-  if lvim.lsp.code_lens.refresh then
-    setup.clear_augroup("lsp_code_lens_refresh")
-  end
+  -- if lvim.lsp.code_lens.refresh then
+  --   setup.clear_augroup("lsp_code_lens_refresh")
+  -- end
 end
 
 function M.common_on_init(client, bufnr)
