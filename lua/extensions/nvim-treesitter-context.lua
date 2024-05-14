cc54098f75bc3e0baed13aa874dd2ca33783ebce
Cenk Kılıç <cenk@kilic.dev>
2024-05-14 15:44:54 +0200

    fix: merge overcomplicated top context

--- lua/extensions/nvim-treesitter-context.lua
+++ lua/extensions/nvim-treesitter-context.lua
@@ -14,10 +14,10 @@ function M.config()
     setup = function()
       return {
         enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        max_lines = 20, -- How many lines the window should span. Values <= 0 mean no limit.
         min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+        multiline_threshold = 5, -- Maximum number of lines to collapse for a single context line
         trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
         mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
