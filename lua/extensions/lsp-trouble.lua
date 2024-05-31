4dd73d2fa032187f9ec550270a101e080bd1f1be
Cenk Kılıç <cenk@kilic.dev>
2024-05-31 10:08:17 +0200

    fix: dont auto preview

--- lua/extensions/lsp-trouble.lua
+++ lua/extensions/lsp-trouble.lua
@@ -40,7 +40,7 @@ function M.config()
         debug = false,
         auto_close = false, -- auto close when there are no items
         auto_open = false, -- auto open when there are items
-        auto_preview = true, -- automatically open preview when on an item
+        auto_preview = false, -- automatically open preview when on an item
         auto_refresh = true, -- auto refresh when open
         focus = false, -- Focus the window when opened
         restore = true, -- restores the last location in the list when opening
