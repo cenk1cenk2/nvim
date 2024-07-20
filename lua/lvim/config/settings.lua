2987719f7378e05405b41492315749f610e4a205
Cenk Kılıç <cenk@kilic.dev>
2024-07-20 14:07:45 +0200

    WIP: .w.

--- lua/lvim/config/settings.lua
+++ lua/lvim/config/settings.lua
@@ -119,6 +119,18 @@ M.load_default_options = function()
         ["*"] = paste("*"),
       },
     }
+
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      },
+      paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+      },
+    }
   end
 
   ---  SETTINGS  ---
