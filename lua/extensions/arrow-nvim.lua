ae0412615b0f3cf0bcbbf91a6d4de39fd8950642
Cenk Kılıç <cenk@kilic.dev>
2024-07-24 10:04:28 +0300

    fix: change index keys

--- lua/extensions/arrow-nvim.lua
+++ lua/extensions/arrow-nvim.lua
@@ -55,7 +55,7 @@ function M.config()
         buffer_leader_key = "m", -- Per Buffer Mappings
         save_key = "cwd", -- what will be used as root to save the bookmarks. Can be also `git_root`.
         global_bookmarks = false, -- if true, arrow will save files globally (ignores separate_by_branch)
-        index_keys = "1234567890!@#$%^&*()", -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
+        -- index_keys = "1234567890!@#$%^&*()", -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
         full_path_list = { "update_stuff" }, -- filenames on this list will ALWAYS show the file path too.
       }
     end,
