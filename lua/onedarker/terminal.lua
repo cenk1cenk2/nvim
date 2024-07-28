20b61dc7c8ceb385f280ba70aecd386edf51b189
Cenk Kılıç <cenk@kilic.dev>
2024-07-28 16:54:42 +0300

    fix: update color scheme

--- lua/onedarker/terminal.lua
+++ lua/onedarker/terminal.lua
@@ -10,7 +10,7 @@ function M.setup()
   vim.g.terminal_color_4 = c.blue[600] -- blue
   vim.g.terminal_color_5 = c.magenta[600] -- purple
   vim.g.terminal_color_6 = c.cyan[600] -- cyan
-  vim.g.terminal_color_7 = c.grey[900] -- white
+  vim.g.terminal_color_7 = c.bg[800] -- white
   vim.g.terminal_color_8 = c.bg[500] -- bright black
   vim.g.terminal_color_9 = c.red[900] -- bright red
   vim.g.terminal_color_10 = c.green[900] -- bright green
@@ -18,7 +18,7 @@ function M.setup()
   vim.g.terminal_color_12 = c.blue[900] -- bright blue
   vim.g.terminal_color_13 = c.magenta[900] -- bright magenta
   vim.g.terminal_color_14 = c.cyan[900] -- bright cyan
-  vim.g.terminal_color_15 = c.grey[900] -- bright grey
+  vim.g.terminal_color_15 = c.bg[900] -- bright grey
   vim.g.terminal_color_16 = c.white -- white
 end
 
