418234dc9011e97613de006b12caca3c5f76604d
Cenk Kılıç <cenk@kilic.dev>
2024-08-25 15:58:27 +0300

    feat: delete dead code

--- lua/ck/plugins/treesitter.lua
+++ lua/ck/plugins/treesitter.lua
@@ -37,42 +37,9 @@ function M.config()
             dependencies = { "nvim-treesitter/nvim-treesitter" },
             event = "InsertEnter",
           },
-          -- {
-          --   "nvim-treesitter/playground",
-          --   after = { "nvim-treesitter" },
-          -- },
         },
       }
     end,
-    -- configure = function(_, fn)
-    -- fn.append_to_setup("comment_nvim", {
-    --   pre_hook = function(ctx)
-    --     if vim.tbl_contains({ "typescriptreact", "vue", "svelte" }, function(type)
-    --       return type ~= vim.bo.filetype
-    --     end) then
-    --       return
-    --     end
-    --
-    --     local U = require("Comment.utils")
-    --
-    --     -- Determine whether to use linewise or blockwise commentstring
-    --     local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-    --
-    --     -- Determine the location where to calculate commentstring from
-    --     local location = nil
-    --     if ctx.ctype == U.ctype.blockwise then
-    --       location = require("ts_context_commentstring.utils").get_cursor_location()
-    --     elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-    --       location = require("ts_context_commentstring.utils").get_visual_start_location()
-    --     end
-    --
-    --     return require("ts_context_commentstring.internal").calculate_commentstring({
-    --       key = type,
-    --       location = location,
-    --     })
-    --   end,
-    -- })
-    -- end,
     setup = function()
       vim.opt.runtimepath:prepend(M.parsers_dir)
 
