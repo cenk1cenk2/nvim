5a1d806e4f4eb9aff76c8bbf6685c7a93e2691e2
Cenk Kılıç <cenk@kilic.dev>
2024-09-07 23:20:23 +0200

    fix: update nui

--- lua/ck/plugins/nui-nvim.lua
+++ lua/ck/plugins/nui-nvim.lua
@@ -14,7 +14,11 @@ function M.config()
         config = false,
         dependencies = {
           -- https://github.com/grapp-dev/nui-components.nvim
-          "grapp-dev/nui-components.nvim",
+          -- "grapp-dev/nui-components.nvim",
+          -- "cenk1cenk2/nui-components.nvim",
+          {
+            dir = "~/development/nui-components.nvim",
+          },
         },
       }
     end,
