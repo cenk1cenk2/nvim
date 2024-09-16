155dc9b933688db3e003fe9a2c67ec4b42a52b33
Cenk Kılıç <cenk@kilic.dev>
2024-09-17 00:49:51 +0200

    fix: update yazi

--- lua/ck/plugins/yazi-nvim.lua
+++ lua/ck/plugins/yazi-nvim.lua
@@ -21,10 +21,10 @@ function M.config()
           open_file_in_vertical_split = "<c-v>",
           open_file_in_horizontal_split = "<c-x>",
           open_file_in_tab = "<c-t>",
-          grep_in_directory = "<c-s>",
-          replace_in_directory = "<c-g>",
+          grep_in_directory = false,
+          replace_in_directory = false,
           cycle_open_buffers = "<c-n>",
-          copy_relative_path_to_selected_files = "<c-y>",
+          copy_relative_path_to_selected_files = false,
           send_to_quickfix_list = "<c-q>",
         },
       }
