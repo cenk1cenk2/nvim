66745e2ba923d9f3cce117dd2ef7d6fec629ede9
Cenk Kılıç <cenk@kilic.dev>
2024-10-20 23:16:29 +0200

    fix: update image name

--- lua/ck/plugins/obsidian-nvim.lua
+++ lua/ck/plugins/obsidian-nvim.lua
@@ -389,7 +389,7 @@ function M.config()
         {
           fn.wk_keystroke({ categories.NOTES, "P" }),
           function()
-            vim.cmd(("ObsidianPasteImg %s"):format(join_paths(require("ck.utils.fs").get_project_buffer_filepath(), "assets/")))
+            vim.cmd(("ObsidianPasteImg %s"):format(join_paths(require("ck.utils.fs").get_project_buffer_dirpath(), "assets/")))
           end,
           desc = "paste image from clipboard",
         },
