1c8e3f683d6353b5342434530840fac884225d44
Cenk Kılıç <cenk@kilic.dev>
2025-05-11 22:21:44 +0200

    fix: update python configuration

--- after/lsp/pyright.lua
+++ after/lsp/pyright.lua
@@ -1,10 +1,10 @@
 ---@type vim.lsp.ClientConfig
 return {
   on_new_config = function(new_config, root_dir)
-    local pipfile = vim.fs.root(root_dir, { "Pipfile" })
-
-    if pipfile then
-      new_config.cmd = { "pipenv", "run", "pyright-langserver", "--stdio" }
-    end
+    -- if vim.fs.root(root_dir, { "uv.lock" }) then
+    --   new_config.cmd = { "uv", "run", "pyright-langserver", "--stdio" }
+    -- elseif vim.fs.root(root_dir, { "Pipfile" }) then
+    --   new_config.cmd = { "pipenv", "run", "pyright-langserver", "--stdio" }
+    -- end
   end,
 }
