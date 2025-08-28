1659abc975bd0a55e40a6bf9b173db21bdb367f7
Cenk Kılıç <cenk@kilic.dev>
2025-08-28 18:43:13 +0200

    fix: update initiation

--- after/lsp/copilot.lua
+++ after/lsp/copilot.lua
@@ -1,5 +1,8 @@
 ---@type vim.lsp.ClientConfig
 return {
+  condition = function()
+    return nvim.lsp.ai.provider.completion == "copilot"
+  end,
   -- filetypes = nvim.lsp.ai.copilot.filetypes
   settings = {
     github = {
