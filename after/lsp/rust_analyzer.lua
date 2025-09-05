89ee01287e91db13bb13c177bd50572544774763
Cenk Kılıç <cenk@kilic.dev>
2025-09-05 02:14:41 +0200

    fix: update rust analyzer config

--- after/lsp/rust_analyzer.lua
+++ after/lsp/rust_analyzer.lua
@@ -3,38 +3,6 @@
 
 ---@type vim.lsp.ClientConfig
 return {
-  -- Needed for inlayHints. Merge this table with your settings or copy
-  -- it from the source if you want to add your own init_options.
-  on_init = function(client, bufnr)
-    -- local name = "rust_tools_nvim"
-    -- local config = nvim.plugins[name]
-    --
-    -- if config.active then
-    -- log:debug "Setup additional rust-tools."
-    --
-    -- rust_tools_config.setup(c)
-    --
-    -- rust_tools.setup_capabilities()
-    -- -- setup on_init
-    -- rust_tools.setup_on_init()
-    -- -- setup root_dir
-    -- rust_tools.setup_root_dir()
-    -- -- setup handlers
-    -- rust_tools.setup_handlers()
-    -- -- setup user commands
-    -- rust_tools.setup_commands()
-    --
-    -- lcommands.setup_lsp_commands()
-    --
-    -- if pcall(require, "dap") then
-    --   rt_dap.setup_adapter()
-    --
-    --   log:debug "Setup additional rust-tools dap."
-    -- end
-    -- end
-
-    require("ck.lsp.handlers").on_init(client, bufnr)
-  end,
   settings = {
     ["rust-analyzer"] = {
       diagnostics = {
