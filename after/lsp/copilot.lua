df36362a529b15d099644e146ca678a0d3db5a13
Cenk Kılıç <cenk@kilic.dev>
2025-09-12 14:36:30 +0200

    fix: update autosuggestions

--- after/lsp/copilot.lua
+++ after/lsp/copilot.lua
@@ -28,27 +28,29 @@ if nvim.lsp.ai.copilot.nes.enabled then
 
       local au = vim.api.nvim_create_augroup("copilotlsp.init", { clear = true })
 
-      --NOTE: NES Completions
-      local debounced_request = require("copilot-lsp.util").debounce(require("copilot-lsp.nes").request_nes, vim.g.copilot_nes_debounce or 500)
-      vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged" }, {
-        callback = function()
-          debounced_request(client)
-        end,
-        group = au,
-      })
+      if nvim.lsp.ai.copilot.nes.auto_suggest then
+        --NOTE: NES Completions
+        local debounced_request = require("copilot-lsp.util").debounce(require("copilot-lsp.nes").request_nes, vim.g.copilot_nes_debounce or 500)
+        vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged" }, {
+          callback = function()
+            debounced_request(client)
+          end,
+          group = au,
+        })
 
-      --NOTE: didFocus
-      vim.api.nvim_create_autocmd("BufEnter", {
-        callback = function()
-          local td_params = vim.lsp.util.make_text_document_params()
-          client:notify("textDocument/didFocus", {
-            textDocument = {
-              uri = td_params.uri,
-            },
-          })
-        end,
-        group = au,
-      })
+        --NOTE: didFocus
+        vim.api.nvim_create_autocmd("BufEnter", {
+          callback = function()
+            local td_params = vim.lsp.util.make_text_document_params()
+            client:notify("textDocument/didFocus", {
+              textDocument = {
+                uri = td_params.uri,
+              },
+            })
+          end,
+          group = au,
+        })
+      end
     end,
   })
 end
