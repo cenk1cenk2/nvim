44faa1de03dad1f6414a12e8bd844765170589ce
Cenk Kılıç <cenk@kilic.dev>
2025-09-19 15:40:15 +0200

    fix: update command

--- after/lsp/vale_ls.lua
+++ after/lsp/vale_ls.lua
@@ -3,15 +3,18 @@ local config = join_paths(vim.fn.stdpath("config"), "utils/linter-config", "vale
 ---@type vim.lsp.ClientConfig
 return {
   filetypes = { "markdown", "plaintext", "text", "gitcommit", "" },
-  -- on_attach = function(client, bufnr)
-  --   require("ck.lsp.handlers").on_attach(client, bufnr)
-  --   require("ck.utils.job")
-  --     .create({
-  --       command = "vale",
-  --       args = { "--config", config, "sync" },
-  --     })
-  --     :start()
-  -- end,
+  on_init = function(client, bufnr)
+    require("ck.lsp.handlers").on_init(client, bufnr)
+    require("ck.utils.job")
+      .create({
+        command = "vale",
+        args = { "--config", config, "sync" },
+        on_failure = function(j, return_val)
+          require("ck.log").error("Vale sync failed: %s", j:result())
+        end,
+      })
+      :start()
+  end,
   init_options = {
     installVale = true,
     configPath = config,
