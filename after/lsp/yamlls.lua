b4427dd17748022331368d10ade9030048aad3a7
Cenk Kılıç <cenk@kilic.dev>
2025-09-19 22:04:02 +0200

    fix: update gitlab files

--- after/lsp/yamlls.lua
+++ after/lsp/yamlls.lua
@@ -81,6 +81,7 @@ return {
         ["http://json.schemastore.org/chart"] = { "Chart.{yml,yaml}" },
         ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
           ".gitlab-ci.{yml,yaml}",
+          "*.gitlab-ci.{yml,yaml}",
         },
         ["https://json.schemastore.org/drone.json"] = { ".drone.yml" },
         ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-playbook.json"] = {
