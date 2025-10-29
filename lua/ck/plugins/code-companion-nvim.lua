a95e98ffb7a67db2b3bece860e3dce0e23a94467
Cenk Kılıç <cenk@kilic.dev>
2025-10-29 22:17:25 +0100

    fix: update codecompanion

--- lua/ck/plugins/code-companion-nvim.lua
+++ lua/ck/plugins/code-companion-nvim.lua
@@ -122,7 +122,7 @@ function M.config()
             tools = {
               groups = {
                 ["default"] = {
-                  prompt = [[I'm giving you access to the ${tools} to help you perform coding tasks. Please use neovim mcp adapter first for any kind of file based operations. Please use fetch for web searches.]],
+                  -- prompt = [[I'm giving you access to the ${tools} to help you perform coding tasks. Please use neovim mcp adapter first for any kind of file based operations. Please use fetch for web searches.]],
                   tools = {
                     "cmd_runner",
                     "create_file",
@@ -399,6 +399,14 @@ function M.config()
             require("codecompanion").toggle()
           end,
           desc = "toggle chat [codecompanion]",
+          mode = { "n", "v" },
+        },
+        {
+          fn.wk_keystroke({ categories.COPILOT, "C" }),
+          function()
+            require("codecompanion").chat()
+          end,
+          desc = "new chat [codecompanion]",
           mode = { "n", "v" },
         },
         {
