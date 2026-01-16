1c75715b9d949c450260bd5c7970f615212d02c8
Cenk Kılıç <cenk@kilic.dev>
2026-01-16 14:43:50 +0100

    fix: update highlights

--- lua/onedarker/highlights.lua
+++ lua/onedarker/highlights.lua
@@ -766,6 +766,13 @@ hl.plugins.copilot_lsp = {
   ["NesApply"] = { link = "DiffText" },
 }
 
+---@type table<string, vim.api.keyset.highlight>
+hl.plugins.mcphub = {
+  MCPHubDiffAdd = { link = "DiffAdd" },
+  MCPHubDiffChange = { link = "DiffChange" }, -- New content being added
+  MCPHubDiffDelete = { link = "DiffDelete" }, -- Content being removed
+}
+
 function M.load_highlights(ns, highlights)
   for group_name, group_settings in pairs(highlights) do
     vim.api.nvim_set_hl(ns, group_name, group_settings)
