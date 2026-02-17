ef85552ec281c4fa8d0affcd7307b29b9c2911c8
Cenk Kılıç <cenk@kilic.dev>
2026-02-17 14:29:15 +0100

    fix: update codediff configuration

--- lua/ck/plugins/codediff-nvim.lua
+++ lua/ck/plugins/codediff-nvim.lua
@@ -11,7 +11,7 @@ function M.config()
       ---@type Plugin
       return {
         "esmuellert/codediff.nvim",
-        -- dir = "~/development/codediff.nvim",
+        dir = "~/development/codediff.nvim",
         dependencies = { "MunifTanjim/nui.nvim" },
         cmd = { "CodeDiff" },
       }
@@ -75,6 +75,7 @@ function M.config()
           hide_merge_artifacts = false,
           original_position = "left",
           conflict_ours_position = "right",
+          conflict_result_position = "center",
         },
         explorer = {
           position = "left",
