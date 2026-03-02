8f4a93b14cf171d4c1e64de9f8a7bf5a223126ea
Cenk Kılıç <cenk@kilic.dev>
2026-03-02 23:53:39 +0100

    fix: go back to upstream

--- lua/ck/plugins/code-companion-nvim.lua
+++ lua/ck/plugins/code-companion-nvim.lua
@@ -10,9 +10,9 @@ function M.config()
     plugin = function()
       ---@type Plugin
       return {
-        -- "olimorris/codecompanion.nvim",
-        "cenk1cenk2/codecompanion.nvim",
-        branch = "patch-4",
+        "olimorris/codecompanion.nvim",
+        -- "cenk1cenk2/codecompanion.nvim",
+        -- branch = "patch-4",
         -- dir = "~/development/codecompanion.nvim",
         cmd = { "CodeCompanion", "CodeCompanionCmd", "CodeCompanionActions", "CodeCompanionChat" },
         keys = { "<Space>c" },
