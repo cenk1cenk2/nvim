8abef571eb82e57a00525d9e070f9e123d26f870
Cenk Kılıç <cenk@kilic.dev>
2026-03-16 15:01:26 +0100

    fix: update copiolot configuration

--- after/lsp/copilot.lua
+++ after/lsp/copilot.lua
@@ -5,7 +5,7 @@ local config = {
   end,
   flags = {
     debounce_text_changes = nvim.lsp.ai.copilot.debounce,
-    allow_incremental_sync = false,
+    allow_incremental_sync = true,
   },
   -- filetypes = nvim.lsp.ai.copilot.filetypes
   settings = {
