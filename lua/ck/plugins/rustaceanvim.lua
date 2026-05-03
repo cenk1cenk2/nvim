5dc4c001aa7d07dcb0af6942be1163940724a8de
Cenk Kılıç <cenk@kilic.dev>
2026-05-03 21:01:14 +0200

    fix: update rustecean nvim

--- lua/ck/plugins/rustaceanvim.lua
+++ lua/ck/plugins/rustaceanvim.lua
@@ -13,6 +13,13 @@ function M.config()
         ft = { "rust" },
       }
     end,
+    on_init = function()
+      vim.g.rustaceanvim = vim.tbl_deep_extend("force", vim.g.rustaceanvim or {}, {
+        server = {
+          auto_attach = false,
+        },
+      })
+    end,
   })
 end
 
