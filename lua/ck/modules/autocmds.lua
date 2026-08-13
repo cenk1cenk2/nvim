d04362427a4fccd6da70e0f04ba0863b28ecabc6
Cenk Kılıç <cenk@kilic.dev>
2026-08-14 00:31:52 +0300

    fix(tmux): pin allow-rename on nvim's own pane
    'set-option -p' with no target resolves to the window's active pane,
    not $TMUX_PANE, so nvim launched into a background pane disabled a
    neighbour's renames and VimLeavePre restored whichever pane happened
    to be active then.

--- lua/ck/modules/autocmds.lua
+++ lua/ck/modules/autocmds.lua
@@ -190,7 +190,7 @@ function M.setup()
           callback = function()
             if vim.env["TMUX_PANE"] then
               os.execute("tmux set-window-option automatic-rename off 2>&1 &")
-              os.execute("tmux set-option -p allow-rename off 2>&1 &")
+              os.execute(("tmux set-option -pt '%s' allow-rename off 2>&1 &"):format(vim.env["TMUX_PANE"]))
             end
           end,
         },
@@ -211,7 +211,7 @@ function M.setup()
           callback = function()
             if vim.env["TMUX_PANE"] then
               os.execute("tmux set-window-option automatic-rename on 2>&1 &")
-              os.execute("tmux set-option -p -u allow-rename 2>&1 &")
+              os.execute(("tmux set-option -pt '%s' -u allow-rename 2>&1 &"):format(vim.env["TMUX_PANE"]))
             end
           end,
         },
